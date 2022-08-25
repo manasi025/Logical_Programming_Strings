e79eded9523d1e9c34930a1079082961c75d8e47
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-08-26 03:10:07 +0530

    Add files via upload

---
+++ DisplayCharacterAtGivenIndexValue.java
@@ -0,0 +1,30 @@
+package stringprblms;
+
+import java.util.Scanner;
+
+public class DisplayCharacterAtGivenIndexValue 
+{
+	public static void main(String[] args)
+	{
+		String str;
+		System.out.print("Enter a 10 letter word : ");
+		Scanner sc = new Scanner(System.in);
+		str = sc.next();
+		DisplayCharacterAtGivenIndexValue(str);
+	}
+
+	private static void DisplayCharacterAtGivenIndexValue(String str) 
+	{
+		// TODO Auto-generated method stub
+		
+		for(int i = 0; i < str.length(); i++)
+		{
+			if(str.charAt(i) != ' ')
+			{
+				i++;
+				break;
+			}
+		}
+		System.out.println("The character displayed at index value 5 = " + str.charAt(5));
+	}
+}
