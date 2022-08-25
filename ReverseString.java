2e8be290cab4ff7312019765fab7df9a0597bf1f
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-08-26 02:10:20 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -4,16 +4,35 @@ import java.util.Scanner;
 
 public class ReverseString 
 {
+	private static char rev = 0;
+
 	public static void main(String[] args) 
 	{
 		String str; 
-		String rev = " ";
+		String revstr = " ";
 		System.out.print("Enter a word : ");
 		Scanner sc = new Scanner(System.in);
 		str = sc.next();
-		
-		for(int i = str.length() -1 ; i >= 0; i--)
-			rev  = rev + str.charAt(i);
-		System.out.println("Reverse string is : " + rev);
+		ReverseString(str);
+	}
+
+	private static void ReverseString(String str) {
+		// TODO Auto-generated method stub
+		String revstr = " ";
+		for(int i = str.length() - 1; i >= 0; i--)
+		{
+			revstr = revstr + str.charAt(i);
+		}
+		System.out.println("Reversed String is = " + revstr);
+		return;
 	}
 }
+
+/*
+
+OUTPUT:
+
+Enter a word : abc
+Reversed String is =  cba
+
+*/
